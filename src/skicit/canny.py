401bae79d9deaa50000de97d4577d6ef41187816
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-07-09 21:22:32 +0430

    added canny working

--- src/skicit/canny.py
+++ src/skicit/canny.py
@@ -2,30 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
-
-from skimage import feature
-
-
-# Generate noisy image of a square
-im = np.zeros((128, 128))
-im[32:-32, 32:-32] = 1
-
-im = ndi.rotate(im, 15, mode='constant')
-im = ndi.gaussian_filter(im, 4)
-im += 0.2 * np.random.random(im.shape)
+from skimage import feature, io
+from skimage.color import rgb2gray
 
 
-im 
-# Compute the Canny filter for two values of sigma
-#feature.canny : Edge filter an image using the Canny algorithm.
-#https://en.wikipedia.org/wiki/Canny_edge_detector
-s1=1.5
-s2=10
+
+sample_file = 'sample.JPG'
+im = io.imread(sample_file)
+im = im[:,:,:]
+im = rgb2gray(im)
+
+#red_edges = feature.canny(im[:, :, 0])
+s1 = 1.5
+s2 = 0.9
 edges1 = feature.canny(im, sigma=s1)
 edges2 = feature.canny(im, sigma=s2)
 
 # display results
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),sharex=True, sharey=True)
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(80, 30),sharex=True, sharey=True)
 
 #plt.show()
 
