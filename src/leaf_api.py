69a1a0e4ea060cd833a31a910b07f71bfafb2c1d
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-07-10 13:45:02 +0430

    added cgi file

--- src/leaf_api.py
+++ src/leaf_api.py
@@ -54,4 +54,5 @@ def calculator():
     finally:
         db.clean_attach(username) #todo : if send multiple times
 
-app.run(host = "0.0.0.0" , port = 80, debug = True)
+#if __name__ == '__main__':
+#    app.run(host = "0.0.0.0" , port = 80, debug = True)
